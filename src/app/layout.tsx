8dd55bf0c15929bca942f5ca2354744537b230ef
athenacats <chenalonya@gmail.com>
2024-10-22 18:55:54 +0300

    Mobile menu not working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import "./styles/index.scss";
 import "./globals.css";
+import "bootswatch/dist/quartz/bootstrap.min.css";
 
 export const metadata: Metadata = {
   title: "Woocommerce Nalela",
@@ -15,9 +16,20 @@ const Layout = (props: any) => {
     <html>
       <head>
         <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
-          href="https://bootswatch.com/5/quartz/bootstrap.min.css"
-        />
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+        ></link>
+        <script
+          defer
+          src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+          integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+        ></script>
+        <script
+          defer
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+          integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+        ></script>
       </head>
       <body>{props.children}</body>
     </html>
