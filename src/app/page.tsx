9bbc433d9e4f82e1c46ca8cf290c5408a323cf9b
athenacats <chenalonya@gmail.com>
2024-10-23 10:42:52 +0300

    Replace product with productindex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header";
-import Product from "./components/Product";
+
+import ProductIndex from "./components/ProductIndex";
 
 export default async function Home() {
   return (
     <>
       <Header />
       <main className="container-fluid">
-        <Product />
+        <ProductIndex />
       </main>
     </>
   );
