42c5db818978d72c3c2564db5046c8b481283073
athenacats <chenalonya@gmail.com>
2024-10-24 14:47:12 +0300

    Fix mobile dropdown not working- missing crossorigin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,14 +29,14 @@ const Layout = (props: any) => {
           integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
         ></link>
         <script
-          defer
           src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
           integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+          crossOrigin="anonymous"
         ></script>
         <script
-          defer
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
           integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+          crossOrigin="anonymous"
         ></script>
       </head>
       <body>{props.children}</body>
