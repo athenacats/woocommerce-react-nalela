fb457e24b81e62bd041a0064a8c8e59fc776547f
athenacats <chenalonya@gmail.com>
2024-10-30 12:33:15 +0300

    Fix product ID not being captured

--- src/app/components/AddToCart.tsx
+++ src/app/components/AddToCart.tsx
@@ -1,9 +1,11 @@
 import { addToCart } from "../helpers/addToCart";
 
 export default function AddToCart(product: any) {
+  console.log(product.product);
+  console.log(product.product.id);
   return (
     <button
-      onClick={() => addToCart(product?.id ?? 0)}
+      onClick={() => addToCart(product.product?.id ?? 0)}
       type="button"
       className="btn btn-primary"
     >
